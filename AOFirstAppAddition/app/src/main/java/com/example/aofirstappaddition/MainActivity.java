317399a5ef5827cd60a856a48c3e8015e6c262b1
ashtonoconnor <ashtonoconnor01@gmail.com>
2019-02-20 12:14:00 -0700

    font change

--- AOFirstAppAddition/app/src/main/java/com/example/aofirstappaddition/MainActivity.java
+++ AOFirstAppAddition/app/src/main/java/com/example/aofirstappaddition/MainActivity.java
@@ -18,6 +18,37 @@ public class MainActivity extends AppCompatActivity {
 
 
 
+
+
+        Button divideBtn = (Button) findViewById(R.id.divideBtn);
+        divideBtn.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                EditText firstNumEditText = (EditText) findViewById(R.id.firstNumEditText);
+                EditText secondNumEditText = (EditText) findViewById(R.id.secondNumEditText);
+                TextView resultTextView = (TextView) findViewById(R.id.resultTextView);
+
+
+                double num1 = parseInt(firstNumEditText.getText().toString());
+                double num2 = parseInt(secondNumEditText.getText().toString());
+                double result = num1 / num2;
+                resultTextView.setText(result + "" );
+            }
+        });
+
+
+
+
+
+
+
+
+
+
+
+
+
+
         Button subtractBtn = (Button) findViewById(R.id.subtractBtn);
         subtractBtn.setOnClickListener(new View.OnClickListener() {
                 @Override
